f68b95a376811bf40536ead3aa2f6367cfde14f6
Eric Sanchez <eric.q.sanchez@gmail.com>
2020-12-31 10:36:50 -0500

    change

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -19,7 +19,7 @@ function HotSlices() {
 export default function HomePage() {
   return (
     <div className="center">
-      <h1>The Best Pizza Downtown!</h1>
+      <h1>The Best Pizza Downtown!!!!!</h1>
       <p>Open 11am to 11pm Every Single Day</p>
       <div>
         <CurrentlySlicing />
